47f520014bef9b78a37e787d2d24f665fde4b127
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2023-01-22 11:50:16 +0530

    Added delete command.

--- setup adb.bat
+++ setup adb.bat
@@ -9,6 +9,7 @@ echo ""
 echo "--------------------------------Downloading adb files--------------------------------"
 powershell Invoke-WebRequest -Uri https://dl.google.com/android/repository/platform-tools_r33.0.3-windows.zip -OutFile lol.zip
 tar -xf lol.zip
+powershell Remove-Item -Path .\lol.zip
 echo "--------------------------------Downloaded--------------------------------"
 cd .\platform-tools\
 set curDir=%cd%
